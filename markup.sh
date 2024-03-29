c42f1186b332f1e94050ed2d880aca93b3d8ee6a
Enrico Sanna <antani@tiscali.it>
2024-03-29 11:31:53 +0100

    adding markup lib

---
+++ markup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+FG_BLACK=$(tput setaf 0)    # black
+FG_BLUE=$(tput setaf 4)     # blue
+FG_CYAN=$(tput setaf 6)     # cyan
+FG_GREEN=$(tput setaf 2)    # green
+FG_MAGENTA=$(tput setaf 5)  # magenta
+FG_RED=$(tput setaf 1)      # red
+FG_WHITE=$(tput setaf 7)    # white
+FG_YELLOW=$(tput setaf 3)   # yellow
+BOLD=$(tput bold)           # bold
+BG_BLACK=$(tput setab 0)    # black
+BG_BLUE=$(tput setab 4)     # blue
+BG_CYAN=$(tput setab 6)     # cyan
+BG_GREEN=$(tput setab 2)    # green
+BG_MAGENTA=$(tput setab 5)  # magenta
+BG_RED=$(tput setab 1)      # red
+BG_WHITE=$(tput setab 7)    # white
+BG_YELLOW=$(tput setab 3)   # yellow
+CLEAR=$(tput sgr0)          # clear
